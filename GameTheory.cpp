74d739547a91b8964f50ffdf8f7dfec3dd458892
AlexeyZaharov <32190965+AlexeyZaharov@users.noreply.github.com>
2022-04-21 11:49:10 +0300

    Update GameTheory.cpp

--- GameTheory.cpp
+++ GameTheory.cpp
@@ -70,7 +70,7 @@ void LR2()
 	double e = -4.8;
 	if (a > 0 || b < 0)
 	{
-		std::cout << "Ñonvex-concave condition is bad!" << std::endl;
+		std::cout << "Ã‘onvex-concave condition is bad!" << std::endl;
 		return;
 	}
 
@@ -79,7 +79,7 @@ void LR2()
 	std::cout << std::endl << std::setw(17) << std::setfill('-') << " " << std::setfill(' ') << std::endl;
 	std::cout << "Analityc method" << std::endl;
 	std::cout << std::setw(17) << std::setfill('-') << " " << std::setfill(' ') << std::endl;
-	std::tie(x, y, H) = AnaliticMethodÑonvexÑoncaveGame(a, b, c, d, e);
+	std::tie(x, y, H) = AnaliticMethodÃ‘onvexÃ‘oncaveGame(a, b, c, d, e);
 
 	std::cout << "X: " << x << std::endl;
 	std::cout << "Y: " << y << std::endl;
@@ -89,11 +89,70 @@ void LR2()
 	std::cout << std::endl << std::setw(17) << std::setfill('-') << " " << std::setfill(' ') << std::endl;
 	std::cout << "Numeric method" << std::endl;
 	std::cout << std::setw(17) << std::setfill('-') << " " << std::setfill(' ') << std::endl;
-	std::tie(x, y, H) = NumericMethodÑonvexÑoncaveGame(a, b, c, d, e);
+	std::tie(x, y, H) = NumericMethodÃ‘onvexÃ‘oncaveGame(a, b, c, d, e);
 
 	std::cout << "X: " << x << std::endl;
 	std::cout << "Y: " << y << std::endl;
 	std::cout << "H: " << H << std::endl;
+}
+
+void LR3()
+{
+	Table tableA = {
+		{5, 7},
+		{11, 6}
+	};
+
+	Matrix matrixA(tableA);
+
+	Table tableB = {
+		{8, 4},
+		{7, 9}
+	};
+
+	Matrix matrixB(tableB);
+
+	std::cout << "Matrix:" << std::endl;
+
+	std::vector<std::vector<std::pair<double, double>>> matrix;
+
+	for (size_t i = 0; i < tableB.size(); ++i)
+	{
+		std::vector<std::pair<double, double>> row;
+		for (size_t j = 0; j < tableB[i].size(); ++j)
+		{
+			row.push_back({tableA[i][j], tableB[i][j]});
+		}
+		matrix.push_back(row);
+	}
+
+	writeTable(matrix, GetNashOptimums(matrixA, matrixB), GetParetoOptimums(matrixA, matrixB));
+
+	std::cout << "\033[31mRED\033[37m - Nash and Pareto optimals" << std::endl;
+	std::cout << "\033[32mGREEN\033[37m - Nash optimals" << std::endl;
+	std::cout << "\033[34mBLUE\033[37m - Pareto optimals" << std::endl;
+
+	Table u_ = {
+		{1, 1}
+	};
+	Table ut_ = {
+		{1},
+		{1}
+	};
+	Matrix u(u_);
+	Matrix ut(ut_);
+
+	double v1 = 1/(u * matrixA.GetRevertMatrix() * ut).GetMatrix()[0][0];
+	Table Y = (v1*matrixA.GetRevertMatrix()*ut).GetMatrix();
+	std::cout << "Y: " << std::endl;
+	writeTable(Y);
+	std::cout << "v1 = " << v1 << std::endl;
+
+	double v2 = 1/(u * matrixB.GetRevertMatrix() * ut).GetMatrix()[0][0];
+	Table X = (v2*u*matrixB.GetRevertMatrix()).GetMatrix();
+	std::cout << "X: " << std::endl;
+	writeTable(X);
+	std::cout << "v2 = " << v2 << std::endl;
 }
 
 int main()
